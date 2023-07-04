b175604a2caac4e724569ecfb6f9ff68c1f1e960
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-07-04 10:56:54 -0300

    [UPDATE] Se refactorizo un poco toda la entidad Comment
    Se cambió el nombre el nombre que recibira la tabla en la base de datos de “comment” a “Comment”
    
    Se cambió el nombre del atributo “comment” a “message” porque no es bien visto que un atributo de la la tabla tenga el mismo nombre que la tabla
    
    Se eliminó la columna “userId” y se colocó directamente el atributo de tipo User.
    
    Se eliminó la columna “proudctId” y se colocó directamente el atributo de tipo Product.

--- src/main/java/com/sensor/entity/Comment.java
+++ src/main/java/com/sensor/entity/Comment.java
@@ -1,20 +1,8 @@
 package com.sensor.entity;
 
 import java.time.LocalDateTime;
-import java.util.Calendar;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.persistence.*;
 
 import com.sensor.security.entity.User;
 import lombok.Data;
@@ -22,7 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-@Table(name ="comment")
+@Table(name ="Comment")
 @Data
 public class Comment {
 
@@ -31,21 +19,17 @@ public class Comment {
 	@Column(name = "id_comment")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long commentId;
-	
-	
-	@Column(name="comment")
-	private String comment;
-	
-	@Column(name = "fk_user")
-	private Long userId;
-	
+
+	@Column(name="message", nullable = false)
+	private String message;
+
 	@ManyToOne
-	@JoinColumn(name="fk_user", insertable = false, updatable = false)
+	@JoinColumn(name="fk_user", nullable = false)
 	private User user;
 	
-	@Column(name = "fk_product")
-	private Long productId;
-
+	@ManyToOne
+	@JoinColumn(name="fk_product", nullable = false)
+	private Product product;
 	
 	@Column(name = "created_date")
 	@CreationTimestamp
