e199f98a3bcdb7dcc16209b0d91b163ea5fb8c9a
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-07-04 11:00:28 -0300

    [UPDATE] Cambiar interface que extiende el repository a JpaRepository
    El equipo considera que esta interface sera mas útil

--- src/main/java/com/sensor/repository/ICommentRepository.java
+++ src/main/java/com/sensor/repository/ICommentRepository.java
@@ -2,11 +2,12 @@ package com.sensor.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sensor.entity.Comment;
 
-public interface ICommentRepository extends CrudRepository<Comment, Long>{
+public interface ICommentRepository extends JpaRepository<Comment, Long> {
 	
 	List<Comment> findByProductId(Long productId);
 
