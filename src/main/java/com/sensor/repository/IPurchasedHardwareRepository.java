41a24b491ec3089bbe35723a82d193b6fb182b4a
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-07-10 14:23:03 -0300

    [UPDATE] Se cambio la interface a JpaRepository porque se consideró mas util para este proyecto

--- src/main/java/com/sensor/repository/IPurchasedHardwareRepository.java
+++ src/main/java/com/sensor/repository/IPurchasedHardwareRepository.java
@@ -1,10 +1,9 @@
 package com.sensor.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.sensor.entity.PurchasedHardware;
 
-public interface IPurchasedHardwareRepository extends CrudRepository<PurchasedHardware, Long> {
+public interface IPurchasedHardwareRepository extends JpaRepository<PurchasedHardware, Long> {
 
 	
 	
