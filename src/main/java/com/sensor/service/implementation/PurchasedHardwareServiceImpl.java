0931e34a044b5d5740d26fe66b944a1896b0ddd7
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-07-10 14:50:24 -0300

    [UPDATE] Se actualizó el service de PurchasedHardware
    1- Se quitó el IUserDao y ahora se usa IUserService, consideramos que cada service debe usar el service de otro componente.
    2-Se quitó el mapper del service porque estamos usando el mapper en el controller.
    3- Los metodos del servicio ya no devuelven DTO sino Entidades.
    4- Se actualizó el metodo "savePurchasedHardware" antes obteniamos al usuario por un atributo que mandaban por el DTO, ahora utilizamos SecurityContextHolder para obtener al usuario que hizo la peticion.
    5- Se actualizó el metodo "deletePurchasedHardwareById" ahora verifica que exista el producto antes de eliminarlo.
    6- Se actualizó el metodo "modifyPurchasedHardwareById" ahora tiene una logica mas compleja, y verifica que él que modifique el producto comprado sea el mismo que lo agregó, ademas de que ya no pide el id de usuario en el DTO, ahora podemos obtenerlo gracias a spring security

--- src/main/java/com/sensor/service/implementation/PurchasedHardwareServiceImpl.java
+++ src/main/java/com/sensor/service/implementation/PurchasedHardwareServiceImpl.java
@@ -4,13 +4,15 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.sensor.security.MainUser;
+import com.sensor.security.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.sensor.dao.IPurchasedHardwareDao;
 import com.sensor.security.dao.IUserDao;
-import com.sensor.dto.purchasedHardware.request.PurchasedHardwareDTO;
 import com.sensor.exception.GeneralException;
 import com.sensor.mapper.PurchasedHardwareMapper;
 import com.sensor.entity.PurchasedHardware;
@@ -24,65 +26,51 @@ public class PurchasedHardwareServiceImpl implements IPurchasedHardwareService {
 	private IPurchasedHardwareDao purchasedHardwareDao;
 
 	@Autowired
-	private IUserDao userDao;
-	
-	@Autowired
-	private PurchasedHardwareMapper purchasedHardwareMapper;
+	private IUserService userService;
 
 	
 	@Override
-	public List<PurchasedHardwareDTO> getAllPurchasedHardware() {
-		return purchasedHardwareDao.getAllPurchasedHardware().stream().map((ph)->purchasedHardwareMapper.toPurchasedHardwareDTO(ph)).collect(Collectors.toList());
+	public List<PurchasedHardware> getAllPurchasedHardware() {
+		return purchasedHardwareDao.getAllPurchasedHardware();
 	}
 
 	@Override
-	public PurchasedHardwareDTO getPurchasedHardwareById(Long purchasedHardwareId) {
-		Optional<PurchasedHardware> opt = purchasedHardwareDao.getPurchasedHardwareById(purchasedHardwareId);
-
-		if (opt.isEmpty()) {
-			throw new GeneralException(HttpStatus.NOT_FOUND,
-					"No se encontro el hardware comprado con el id : " + purchasedHardwareId);
-		}
-		return purchasedHardwareMapper.toPurchasedHardwareDTO(opt.get());
+	public PurchasedHardware getPurchasedHardwareById(Long purchasedHardwareId) {
+		return purchasedHardwareDao.getPurchasedHardwareById(purchasedHardwareId).orElseThrow(() -> new GeneralException(HttpStatus.NOT_FOUND,
+				"No se encontro el hardware comprado con el id : " + purchasedHardwareId));
 	}
 
 	@Override
-	public void savePurchasedHardware(PurchasedHardwareDTO purchasedHardwareDTO) {
-
-		Optional<User> user = userDao.getUserById(purchasedHardwareDTO.getUserId());
-
-		if (user.isEmpty()) {
-			throw new GeneralException(HttpStatus.NOT_FOUND, "No se encontro el usuario con id : " + purchasedHardwareDTO.getUserId());
-		}
-		
-		purchasedHardwareDao.savePurchasedHardware(purchasedHardwareMapper.toPurchasedHardware(purchasedHardwareDTO));
+	public void savePurchasedHardware(PurchasedHardware purchasedHardware) {
+		MainUser mu = (MainUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		User user = this.userService.getUserByEmail(mu.getUsername());
+		purchasedHardware.setUser(user);
+		purchasedHardwareDao.savePurchasedHardware(purchasedHardware);
 	}
 
 	@Override
 	public void deletePurchasedHardwareById(Long purchasedHardwareId) {
-		Optional<PurchasedHardware> opt = purchasedHardwareDao.getPurchasedHardwareById(purchasedHardwareId);
-
-		if (opt.isEmpty()) {
-			throw new GeneralException(HttpStatus.NOT_FOUND,
-					"No se encontro el hardware comprado con id : " + purchasedHardwareId);
-		}
-
+		this.getPurchasedHardwareById(purchasedHardwareId);
 		purchasedHardwareDao.deletePurchasedHardwareById(purchasedHardwareId);
 	}
 
 
 	@Override
-	public void modifyPurchasedHardwareById(Long purchasedHardwareId, PurchasedHardwareDTO purchasedHardwareDTO) {
-		
-		Optional<User> user = userDao.getUserById(purchasedHardwareDTO.getUserId());
-		
-		if (user.isEmpty()) {
-			throw new GeneralException(HttpStatus.NOT_FOUND, "No se encontro el usuario con id : " + purchasedHardwareDTO.getUserId());
+	public void modifyPurchasedHardwareById(Long purchasedHardwareId, PurchasedHardware purchasedHardware) {
+		MainUser mu = (MainUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		PurchasedHardware purchasedHardwareSearched = this.getPurchasedHardwareById(purchasedHardwareId);
+		if(!purchasedHardwareSearched.getUser().getUserId().equals(mu.getId())){
+			throw new GeneralException(HttpStatus.UNAUTHORIZED, "Solo el usuario que creó este producto comprado puede modificarlo");
 		}
-		
-		
-		purchasedHardwareDao.savePurchasedHardware(purchasedHardwareMapper.toPurchasedHardwareModify(purchasedHardwareDTO));
-		
+		purchasedHardwareSearched.setName(purchasedHardware.getName());
+		purchasedHardwareSearched.setQuantity(purchasedHardware.getQuantity());
+		purchasedHardwareSearched.setDatePurchase(purchasedHardware.getDatePurchase());
+		purchasedHardwareSearched.setProvider(purchasedHardware.getProvider());
+		purchasedHardwareSearched.setPrice(purchasedHardware.getPrice());
+
+		purchasedHardwareDao.savePurchasedHardware(purchasedHardwareSearched);
+
 	}
 
 }
