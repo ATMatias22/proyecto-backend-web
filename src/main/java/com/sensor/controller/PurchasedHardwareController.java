f34b63d87ecf0fc94bd71c2563e2fffd82822e16
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-07-10 15:46:37 -0300

    [UPDATE] Se actualizó el Controller de PurchasedHardware
    1- Se cambió el nombre del endpoint de "purchasedhardwares"  a "purchased-hardwares" porque es la nomenclatura que estamos utilizando en los endpoints
    2- Se agregó el mapper para mapear entidad a DTO y viceversa

--- src/main/java/com/sensor/controller/PurchasedHardwareController.java
+++ src/main/java/com/sensor/controller/PurchasedHardwareController.java
@@ -1,7 +1,11 @@
 package com.sensor.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.sensor.dto.purchasedHardware.request.PurchasedHardwareRequest;
+import com.sensor.dto.purchasedHardware.response.PurchasedHardwareResponse;
+import com.sensor.mapper.PurchasedHardwareMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,49 +20,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sensor.dto.purchasedHardware.request.PurchasedHardwareDTO;
 import com.sensor.service.IPurchasedHardwareService;
 
 @RestController
 @PreAuthorize("hasRole('ADMIN')")
-@RequestMapping("/purchasedhardwares")
+@RequestMapping("/purchased-hardwares")
 @CrossOrigin(origins = "*")
 public class PurchasedHardwareController {
 	
 	
 	@Autowired
 	private IPurchasedHardwareService purchasedHardwareService;
+
+	@Autowired
+	private PurchasedHardwareMapper purchasedHardwareMapper;
 	
 	@GetMapping("/all")
-	public ResponseEntity<List<PurchasedHardwareDTO>> getAllPurchasedHardware() {
-		return new ResponseEntity<>(purchasedHardwareService.getAllPurchasedHardware(), HttpStatus.OK);
+	public ResponseEntity<List<PurchasedHardwareResponse>> getAllPurchasedHardware() {
+		return new ResponseEntity<>(purchasedHardwareService.getAllPurchasedHardware().stream().map((ph) -> this.purchasedHardwareMapper.toPurchasedHardwareResponse(ph)).collect(Collectors.toList()), HttpStatus.OK);
 	}
 	
 	
 	@GetMapping("/{purchasedHardwareServiceId}")
-	public ResponseEntity<PurchasedHardwareDTO> getPurchasedHardwareById(
+	public ResponseEntity<PurchasedHardwareResponse> getPurchasedHardwareById(
 			@PathVariable("purchasedHardwareServiceId") Long purchasedHardwareServiceId) {
-		return new ResponseEntity<PurchasedHardwareDTO>(purchasedHardwareService.getPurchasedHardwareById(purchasedHardwareServiceId), HttpStatus.OK);
+		return new ResponseEntity<>(this.purchasedHardwareMapper.toPurchasedHardwareResponse(purchasedHardwareService.getPurchasedHardwareById(purchasedHardwareServiceId)), HttpStatus.OK);
 	}
 	
 	
 	
 	@PostMapping
-	public ResponseEntity<PurchasedHardwareDTO> savePurchasedHardware(@RequestBody PurchasedHardwareDTO purchasedHardwareDTO) {
-		purchasedHardwareService.savePurchasedHardware(purchasedHardwareDTO);
+	public ResponseEntity<Void> savePurchasedHardware(@RequestBody PurchasedHardwareRequest purchasedHardwareRequest) {
+		purchasedHardwareService.savePurchasedHardware(this.purchasedHardwareMapper.toPurchasedHardware(purchasedHardwareRequest));
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping("/{purchasedHardwareId}")
-	public ResponseEntity deletePurchasedHardwareById(@PathVariable("purchasedHardwareId") Long purchasedHardwareId) {
+	public ResponseEntity<Void> deletePurchasedHardwareById(@PathVariable("purchasedHardwareId") Long purchasedHardwareId) {
 		purchasedHardwareService.deletePurchasedHardwareById(purchasedHardwareId);
-		return new ResponseEntity(HttpStatus.NO_CONTENT);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@PatchMapping("/{purchasedHardwareId}")
-	public ResponseEntity modifyPurchasedHardwareById(@PathVariable("purchasedHardwareId") Long purchasedHardwareId, @RequestBody PurchasedHardwareDTO purchasedHardwareDTO) {
-		purchasedHardwareService.modifyPurchasedHardwareById(purchasedHardwareId,purchasedHardwareDTO);
-		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	public ResponseEntity<Void> modifyPurchasedHardwareById(@PathVariable("purchasedHardwareId") Long purchasedHardwareId, @RequestBody PurchasedHardwareRequest purchasedHardwareRequest) {
+		purchasedHardwareService.modifyPurchasedHardwareById(purchasedHardwareId, this.purchasedHardwareMapper.toPurchasedHardware(purchasedHardwareRequest));
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 
