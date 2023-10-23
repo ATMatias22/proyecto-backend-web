3cf0adbd17f7204ab20f1a4c71d3f1c129b00d27
ATMatias22 <matias.torrez@davinci.edu.ar>
2023-10-23 15:13:41 -0300

    Evitando dependencia circular entre IUserService y IFavoriteService
    Necesitamos usar el IFavoriteService en UserServiceImpl y como la implementacion de IFavoriteService tenia como atributo la implementacion de IUserService esto iba a terminar en una dependencia circular.
    Para evitar esto lo que hicimos fue sacar la implementacion de IUserService de la implementacion de IFavoriteService pero como necesitabamos esa implementacion de IUserService para guardar un favorito, lo que hicimos fue cambiar los metodos "saveFavorite","deleteFirstFavoriteByUserAndProduct" y " getAllFavoritesByUserLoggedIn", agregandoles un parametro que seria el usuario logueado, entonces ahora el FavoriteController es el que tiene el atributo IUserService entonces en el controller hacemos la obtencion del usuario logueado y se la pasamos a los metodos "saveFavorite","deleteFirstFavoriteByUserAndProduct" y " getAllFavoritesByUserLoggedIn", asi evitamos la dependencia cirucular.

--- src/main/java/com/sensor/controller/FavoriteController.java
+++ src/main/java/com/sensor/controller/FavoriteController.java
@@ -3,12 +3,16 @@ package com.sensor.controller;
 
 import com.sensor.dto.favorite.response.FavoriteResponse;
 import com.sensor.mapper.FavoriteMapper;
+import com.sensor.security.MainUser;
+import com.sensor.security.entity.User;
+import com.sensor.security.service.IUserService;
 import com.sensor.service.IFavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,26 +27,38 @@ public class FavoriteController {
     private IFavoriteService favoriteService;
 
     @Autowired
+    private IUserService userService;
+
+    @Autowired
     private FavoriteMapper favoriteMapper;
 
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/all", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<FavoriteResponse>> getAllFavoritesByUserLoggedIn() {
-        return new ResponseEntity<>(this.favoriteService.getAllFavoritesByUserLoggedIn().stream().map( fav -> this.favoriteMapper.toFavoriteResponse(fav)).collect(Collectors.toList()), HttpStatus.OK);
+
+        MainUser mu = (MainUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userLoggedIn = this.userService.getUserByEmail(mu.getUsername());
+
+        return new ResponseEntity<>(this.favoriteService.getAllFavoritesByUserLoggedIn(userLoggedIn).stream().map( fav -> this.favoriteMapper.toFavoriteResponse(fav)).collect(Collectors.toList()), HttpStatus.OK);
     }
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping(value = "/products/{productId}")
     public ResponseEntity<Void> saveFavorite(@PathVariable("productId") Long productId) {
-        this.favoriteService.saveFavorite(this.favoriteMapper.toFavoriteTransportToService(productId));
+
+        MainUser mu = (MainUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userLoggedIn = this.userService.getUserByEmail(mu.getUsername());
+        this.favoriteService.saveFavorite(this.favoriteMapper.toFavoriteTransportToService(productId),userLoggedIn);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PreAuthorize("isAuthenticated()")
     @DeleteMapping(value = "/products/{productId}")
     public ResponseEntity<Void> deleteFavoriteByUserAndProduct(@PathVariable("productId") Long productId) {
-        this.favoriteService.deleteFirstFavoriteByUserAndProduct(this.favoriteMapper.toFavoriteTransportToService(productId));
+        MainUser mu = (MainUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userLoggedIn = this.userService.getUserByEmail(mu.getUsername());
+        this.favoriteService.deleteFirstFavoriteByUserAndProduct(this.favoriteMapper.toFavoriteTransportToService(productId),userLoggedIn);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
